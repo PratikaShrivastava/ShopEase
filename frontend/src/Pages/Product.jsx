64f72999e772cb707ad5011143fd85f165fa1f6c
PratikaShrivastava <pratikajsr@gmail.com>
2024-09-23 20:30:01 +0530

    initial commit

---
+++ frontend/src/Pages/Product.jsx
@@ -0,0 +1,28 @@
+import React, { useContext, useEffect, useState } from 'react'
+import Breadcrums from '../Components/Breadcrums/Breadcrums'
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox'
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../Context/ShopContext'
+
+const Product = () => {
+  const {products} = useContext(ShopContext);
+  const {productId} = useParams();
+  const [product,setProduct] = useState(false);
+
+  useEffect(()=>{
+    setProduct(products.find((e)=>e.id === Number(productId)))
+  },[products,productId])
+
+  return product ? (
+    <div>
+      <Breadcrums product={product}/>
+      <ProductDisplay product={product}/>
+      <DescriptionBox/>
+      <RelatedProducts id={product.id} category={product.category}/>
+    </div>
+  ) : null
+}
+
+export default Product
